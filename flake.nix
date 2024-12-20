aec8942eee4cf2ce09ab1682841a75469c53747e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2024-12-21 02:33:07 +0300

    build(nix): add gateway service to process-compose to run

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
     systems.url = "github:nix-systems/x86_64-linux";
     process-compose.url = "github:Platonic-Systems/process-compose-flake";
     services.url = "github:juspay/services-flake";
+    d2n.url = "github:nix-community/dream2nix/rust-cargo-vendor/git-replace-workspace-attributes";
+    d2n.inputs.nixpkgs.follows = "nixpkgs";
+    nci.url = "github:yusdacra/nix-cargo-integration";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
+    nci.inputs.dream2nix.follows = "d2n";
   };
 
   outputs =
@@ -15,12 +20,13 @@
       systems = import inp.systems;
       imports = [
         inp.process-compose.flakeModule
+        inp.nci.flakeModule
       ];
       perSystem =
         {
+          config,
           self',
           pkgs,
-          lib,
           system,
           ...
         }:
@@ -29,29 +35,39 @@
           pluralkitConfCheck = ''
             [[ -f "pluralkit.conf" ]] || (echo "pluralkit config not found, please copy pluralkit.conf.example to pluralkit.conf and edit it" && exit 1)
           '';
+          sourceDotenv = ''
+            [[ -f ".env" ]] && echo "sourcing .env file..." && export "$(xargs < .env)"
+          '';
+
+          rustOutputs = config.nci.outputs;
+          composeCfg = config.process-compose."dev";
         in
         {
           _module.args.pkgs = import inp.nixpkgs {
             inherit system;
-            config.permittedInsecurePackages = ["dotnet-sdk-6.0.428"];
+            config.permittedInsecurePackages = [ "dotnet-sdk-6.0.428" ];
           };
+
+          nci.toolchainConfig = {
+            channel = "nightly";
+          };
+          nci.projects."pluralkit-services" = {
+            path = ./.;
+          };
+          nci.crates."gateway" = {
+            depsDrvConfig.mkDerivation = {
+              nativeBuildInputs = [ pkgs.protobuf ];
+            };
+            drvConfig.mkDerivation = {
+              nativeBuildInputs = [ pkgs.protobuf ];
+            };
+          };
+
           formatter = pkgs.nixfmt-rfc-style;
           devShells = {
-            pluralkit = pkgs.mkShellNoCC {
-              packages = with pkgs; [
-                cargo rust-analyzer rustfmt
-                gcc
-                protobuf
-                dotnet-sdk_6
-                omnisharp-roslyn
-                go
-                nodejs yarn
-              ];
-
-              NODE_OPTIONS = "--openssl-legacy-provider";
-            };
-            default = self'.devShells.pluralkit;
+            default = rustOutputs."pluralkit-services".devShell;
           };
+
           process-compose."dev" = {
             imports = [ inp.services.processComposeModules.default ];
 
@@ -75,36 +91,62 @@
               '';
             };
 
-            settings.processes = {
-              pluralkit-bot = {
-                command = pkgs.writeShellApplication {
-                  name = "pluralkit-bot";
-                  runtimeInputs = with pkgs; [coreutils podman];
-                  text = ''
-                    set -x
-                    ${pluralkitConfCheck}
-                    echo "building pluralkit docker image"
-                    podman build --tag='pluralkit' .
-                    mkdir -p "${dataDir}/pluralkit/log"
-                    exec podman run -t --volume ./pluralkit.conf:/app/pluralkit.conf:ro --volume ${dataDir}/pluralkit/log:/var/log/pluralkit --net=host 'pluralkit'
-                  '';
+            settings.processes =
+              let
+                procCfg = composeCfg.settings.processes;
+              in
+              {
+                pluralkit-bot = {
+                  command = pkgs.writeShellApplication {
+                    name = "pluralkit-bot";
+                    runtimeInputs = with pkgs; [
+                      coreutils
+                      podman
+                    ];
+                    text = ''
+                      ${sourceDotenv}
+                      set -x
+                      ${pluralkitConfCheck}
+                      podman build --tag='pluralkit' .
+                      mkdir -p "${dataDir}/pluralkit/log"
+                      exec podman run -t --volume ./pluralkit.conf:/app/pluralkit.conf:ro --volume ${dataDir}/pluralkit/log:/var/log/pluralkit --net=host 'pluralkit'
+                    '';
+                  };
+                  depends_on.postgres.condition = "process_healthy";
+                  depends_on.redis.condition = "process_healthy";
+                  depends_on.pluralkit-gateway.condition = "process_healthy";
+                  shutdown.signal = 9; # KILL
                 };
-                depends_on.postgres.condition = "process_healthy";
-                depends_on.redis.condition = "process_healthy";
-                shutdown.signal = 9; # KILL
+                pluralkit-gateway =
+                  let
+                    shell = rustOutputs."gateway".devShell;
+                  in
+                  {
+                    command = pkgs.writeShellApplication {
+                      name = "pluralkit-gateway";
+                      runtimeInputs =
+                        (with pkgs; [
+                          curl
+                          gnugrep
+                          coreutils
+                          protobuf
+                          shell.stdenv.cc
+                        ])
+                        ++ shell.nativeBuildInputs;
+                      text = ''
+                        ${sourceDotenv}
+                        set -x
+                        ${pluralkitConfCheck}
+                        exec cargo run --package gateway
+                      '';
+                    };
+                    depends_on.postgres.condition = "process_healthy";
+                    depends_on.redis.condition = "process_healthy";
+                    # configure health checks
+                    liveness_probe.exec.command = ''curl -s -o /dev/null -w "%{http_code}" http://localhost:5000/stats | grep "302"'';
+                    readiness_probe.exec.command = procCfg.pluralkit-gateway.liveness_probe.exec.command;
+                  };
               };
-              # pluralkit-gateway = {
-              #   command = pkgs.writeShellApplication {
-              #     name = "pluralkit-gateway";
-              #     runtimeInputs = with pkgs; [coreutils];
-              #     text = ''
-              #       set -x
-              #       ${pluralkitConfCheck}
-              #       exec 
-              #     '';
-              #   };
-              # };
-            };
           };
         };
     };
